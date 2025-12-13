57d605c0ffeba0327bde52e811d6788370adaba3
zlccx <zhang_lichen@qq.com>
2025-12-13 23:44:17 +0800

    完善三种排序功能，智能排序为根据BASE_DIST分组后按照free排序

--- pages/index/index.js
+++ pages/index/index.js
@@ -4,17 +4,18 @@ const app = getApp();
 const PI = 3.1415926535897932384626;
 const EARTH_R = 6378245.0;  // 地球长半轴
 const EE = 0.00669342162296594323;  // 偏心率平方
+const BASE_DIST = 500; // 设定为 500m 为一组
 Page({
     data: {
         providers: [],
         user_location: null,
         stations: null,
-        sortBy: 1,
+        sortBy: 0,
         sortText: ['智能排序', '按距离排序', '按空位排序'],
         markers: [],
     },
 
-    // 用 Haversine 公式计算
+    // 用 Haversine 公式计算经纬度距离
     calculateDistance(lat1, lng1, lat2, lng2) {
         const R = 6371000; // 地球半径(米)
         const toRad = (d) => d * Math.PI / 180;
@@ -32,6 +33,7 @@ Page({
         return Math.round(R * c);
     },
 
+    // 将bd09坐标转换成Gcj02坐标
     bd09ToGcj02(bd_lon, bd_lat) {
         const X_PI = (Math.PI * 3000.0) / 180.0;
         const x = bd_lon - 0.0065;
@@ -43,6 +45,30 @@ Page({
         const gcj_lat = z * Math.sin(theta);
         
         return [gcj_lon, gcj_lat];
+    },
+
+    smart_sort() {
+        let stations = this.data.stations;
+        let divided_stations = [];
+        let zero = [];
+        for (let u of stations) {
+            for (let i = divided_stations.length; i <= u.dist / BASE_DIST; i ++ )
+                divided_stations.push([]);
+            console.log("12.13 -> ", parseInt(u.dist/BASE_DIST), divided_stations.length);
+            if (u.free) divided_stations[parseInt(u.dist / BASE_DIST)].push(u);
+            else zero.push(u);
+        }
+        let new_stations = [];
+        for (let u of divided_stations) {
+            u.sort((a, b) => b.free - a.free);
+            for (let v of u)
+                new_stations.push(v);
+        }
+        for (let u of zero) new_stations.push(u);
+        console.log(new_stations);
+        this.setData({
+            stations: new_stations,
+        })
     },
 
     transport_data: async function () { // omg 我终于会处理异步了
@@ -58,12 +84,15 @@ Page({
             this.data.stations[i].dist =
                 this.calculateDistance(u.lat, u.lon, user_location.latitude, user_location.longitude);
         }
-        this.data.stations.sort((a, b) => a.dist - b.dist);
+        
         this.setData({
             user_location: user_location,
             providers: providers,
             stations: status.stations,
         });
+
+        this.smart_sort();
+
         let i = 0;
         for (let u of this.data.stations) {
             this.data.markers.push({
@@ -82,8 +111,18 @@ Page({
 
     changeSort() {
         this.data.sortBy = (this.data.sortBy + 1) % 3;
+        if (this.data.sortBy == 0) {
+            this.smart_sort();
+        }
+        if (this.data.sortBy == 1) { // 按距离排序
+            this.data.stations.sort((a, b) => a.dist - b.dist);
+        }
+        else if (this.data.sortBy == 2) { // 按空位排序
+            this.data.stations.sort((a, b) => b.free - a.free);
+        }
         this.setData({
             sortBy: this.data.sortBy,
+            stations: this.data.stations,
         })
     },
 
